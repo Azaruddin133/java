f88b5edf1b3e497604ba0e399621bbe95278d5d0
AZARUDDIN133 <azaruddinshaik621@gmail.com>
2024-10-30 18:36:26 +0530

    add1

---
+++ Demo1.java
@@ -0,0 +1,28 @@
+package Map;
+import java.util.ArrayList;
+
+public class Demo1 {
+
+	public static void main(String[] args)
+	{
+		
+		 ArrayList<Integer> list = new ArrayList<>();
+	        list.add(10);
+	        list.add(20);
+	        list.add(30);
+	        list.add(40);
+	        list.add(10);
+
+	        for(Integer s : list)
+	        {
+	           
+	        }
+
+	      
+	        
+	      
+	}
+
+	
+
+}
---
+++ Sample3.java
@@ -0,0 +1,11 @@
+package Map;
+
+public class Sample3 
+{
+	public static void main(String[] args) 
+	{
+		System.err.println("sample3");
+		
+	}
+
+}
